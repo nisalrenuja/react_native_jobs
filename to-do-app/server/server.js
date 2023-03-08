0509d373780bff95f058a0db411e0f7525748877
nisalrenuja <nrenuja@gmail.com>
2023-03-08 16:35:54 +0530

    add get route

--- to-do-app/server/server.js
+++ to-do-app/server/server.js
@@ -5,7 +5,10 @@ const pool = require("./db");
 
 app.get("/todos", async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todos");
+    const allTodos = await pool.query(
+      "SELECT * FROM todos WHERE user_email = $1",
+      [""]
+    );
     res.json(allTodos.rows);
   } catch (err) {
     console.error(err.message);
