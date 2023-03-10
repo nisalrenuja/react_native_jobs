1835ddac3686966c1359964a1f606c0bfac2a114
nisalrenuja <nrenuja@gmail.com>
2023-03-11 02:41:38 +0530

    get data function added

--- to-do-app/client/src/App.js
+++ to-do-app/client/src/App.js
@@ -1,6 +1,21 @@
 import ListHeader from "./components/ListHeader";
+import { useEffect } from "react";
 
 const App = () => {
+  const user_email = "nrenuja@gmail.com";
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${user_email}`);
+      const jasonData = await response.json();
+      console.log(jasonData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="app">
       <ListHeader listName={"🏝️ Holiday check list"} />
