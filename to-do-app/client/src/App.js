4861c1527a5ee8bb5793afa9987dc87b08bf747a
nisalrenuja <nrenuja@gmail.com>
2023-03-11 20:55:29 +0530

    sort task list

--- to-do-app/client/src/App.js
+++ to-do-app/client/src/App.js
@@ -1,13 +1,15 @@
 import ListHeader from "./components/ListHeader";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ListItem from "./components/ListItem";
 
 const App = () => {
   const user_email = "nrenuja@gmail.com";
+  const [tasks, setTasks] = useState(null);
   const getData = async () => {
     try {
       const response = await fetch(`http://localhost:8000/todos/${user_email}`);
       const jasonData = await response.json();
-      console.log(jasonData);
+      setTasks(jasonData);
     } catch (err) {
       console.error(err);
     }
@@ -16,9 +18,18 @@ const App = () => {
   useEffect(() => {
     getData();
   }, []);
+
+  console.log(tasks);
+
+  //sorted list
+  const sortedList = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date));
+
   return (
     <div className="app">
       <ListHeader listName={"🏝️ Holiday check list"} />
+      {sortedList?.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
     </div>
   );
 };
