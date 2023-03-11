b98e44b52395e74d84f6ccdb378d7a5512e10158
nisalrenuja <nrenuja@gmail.com>
2023-03-11 20:56:04 +0530

    components added

--- to-do-app/client/src/components/ListItem.js
+++ to-do-app/client/src/components/ListItem.js
@@ -1,5 +1,20 @@
-const ListItem = () => {
-  return <div></div>;
+import TickIcon from "./TickIcon";
+import ProgressBar from "./ProgressBar";
+
+const ListItem = ({ task }) => {
+  return (
+    <li className="list-item">
+      <div className="info-container">
+        <TickIcon />
+        <h3 className="task-title">{task.title}</h3>
+        <ProgressBar />
+      </div>
+      <div className="button-container">
+        <button className="edit-button">Edit</button>
+        <button className="delete-button">Delete</button>
+      </div>
+    </li>
+  );
 };
 
 export default ListItem;
