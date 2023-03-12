74a4016d34da744a0fa67bd6f05c786ec9cf10e4
nisalrenuja <nrenuja@gmail.com>
2023-03-13 03:37:27 +0530

    show modal use state added

--- to-do-app/client/src/components/ListItem.js
+++ to-do-app/client/src/components/ListItem.js
@@ -1,7 +1,10 @@
 import TickIcon from "./TickIcon";
 import ProgressBar from "./ProgressBar";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const ListItem = ({ task }) => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <li className="list-item">
       <div className="info-container">
@@ -10,9 +13,12 @@ const ListItem = ({ task }) => {
         <ProgressBar />
       </div>
       <div className="button-container">
-        <button className="edit-button">Edit</button>
+        <button className="edit-button" onClick={() => setShowModal(true)}>
+          Edit
+        </button>
         <button className="delete-button">Delete</button>
       </div>
+      {showModal && <Modal mode={"edit"} setShowModal={setShowModal} />}
     </li>
   );
 };
