3bc4ec8253f7e71fc546dd0578c3869df61a6d44
nisalrenuja <nrenuja@gmail.com>
2023-03-13 03:37:10 +0530

    modal added

--- to-do-app/client/src/components/ListHeader.js
+++ to-do-app/client/src/components/ListHeader.js
@@ -1,4 +1,8 @@
+import Modal from "./Modal";
+import { useState } from "react";
+
 const ListHeader = ({ listName }) => {
+  const [showModal, setShowModal] = useState(null);
   const signOut = () => {
     console.log("sign out");
   };
@@ -7,10 +11,13 @@ const ListHeader = ({ listName }) => {
     <div className="list-header">
       <h1>{listName}</h1>
       <div className="button-container">
-        <button className="create">Add New</button>
+        <button className="create" onClick={() => setShowModal(true)}>
+          Add New
+        </button>
         <button className="signOut" onClick={signOut}>
           Sign Out
         </button>
+        {showModal && <Modal mode={"create"} setShowModal={setShowModal} />}
       </div>
     </div>
   );
