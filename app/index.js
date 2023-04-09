e45b462309522ae58d65ef34d03e2748b5ac4b07
nisalrenuja <nrenuja@gmail.com>
2023-04-09 19:41:25 +0530

    home screen added

--- app/index.js
+++ app/index.js
@@ -8,12 +8,39 @@ import {
   ScreenHeaderBtn,
   Welcome,
 } from "../components";
+import { ScreenStack } from "react-native-screens";
 
 const Home = () => {
   const router = useRouter();
   return (
-    <SafeAreaView>
-      <Text>Welcome to the Home Screen!</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: () => "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
