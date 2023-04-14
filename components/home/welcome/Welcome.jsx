f8e527939a3061736626546265609283f88be712
nisalrenuja <nrenuja@gmail.com>
2023-04-14 23:58:45 +0530

    active jobs added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,8 +11,11 @@ import { useRouter } from "expo-router";
 import { icons, SIZES } from "../../../constants";
 import styles from "./welcome.style";
 
+const jobTypes = ["Full Time", "Part Time", "Contract"];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full Time");
   return (
     <View>
       <View style={styles.container}>
@@ -28,6 +31,33 @@ const Welcome = () => {
             placeholder="What are you looking for?"
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   );
